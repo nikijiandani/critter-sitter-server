8b6c9cee0d75ab410565f4b95e7628cfcbdeefc1
Nikita Jiandani <niki.jiandani@gmail.com>
2019-07-19 17:46:52 -0400

    Add new gitignore , fix dependencies, init setup

--- server.js
+++ server.js
@@ -5,17 +5,18 @@ require('dotenv').config();
 const PORT        = process.env.PORT || 8080;
 const ENV         = process.env.ENV || "development";
 const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
 const app         = express();
 
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
+const cors        = require('cors');
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
+
+app.use(cors());
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -24,16 +25,6 @@ app.use(morgan('dev'));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
-
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use("/styles", sass({
-  src: __dirname + "/styles",
-  dest: __dirname + "/public/styles",
-  debug: true,
-  outputStyle: 'expanded'
-}));
-app.use(express.static("public"));
 
 // Mount all resource routes
 app.use("/api/users", usersRoutes(knex));
