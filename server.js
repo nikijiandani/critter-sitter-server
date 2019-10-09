3bf87f0098cc71dc3b6f4f783dff00a559015581
Nikita Jiandani <niki.jiandani@gmail.com>
2019-10-08 21:14:44 -0400

    rm dotenv config

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 "use strict";
 
-require('dotenv').config();
+// require("dotenv").config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const app         = express();
+const PORT = process.env.PORT || 8080;
+const ENV = process.env.ENV || "development";
+const express = require("express");
+const app = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
-const cors        = require('cors');
-const bodyParser  = require("body-parser");
+const knexConfig = require("./knexfile");
+const knex = require("knex")(knexConfig[ENV]);
+const morgan = require("morgan");
+const knexLogger = require("knex-logger");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 // this must be before other app.use statements
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(bodyParser.json());
 
 // Seperated Routes for each Resource
 const usersRoute = require("./routes/users");
@@ -30,7 +32,7 @@ app.use(cors());
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
@@ -43,7 +45,7 @@ app.use("/api/reviews", reviewsRoute(knex));
 // Home page
 app.get("/", (req, res) => {
   res.json({
-    user: 'Lighthouse Labs'
+    user: "Lighthouse Labs"
   });
 });
 
